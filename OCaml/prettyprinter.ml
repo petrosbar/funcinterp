6fc2f48083bcb122758fa3db7803b7361ab28c54
Petros Barbagiannis <pbarb@math.uoa.gr>
2017-01-23 16:41:59 +0200

    Comments

--- OCaml/prettyprinter.ml
+++ OCaml/prettyprinter.ml
@@ -1,6 +1,24 @@
+(*
+ * A pretty-printer for the defining language.
+ *
+ * An example:
+ *  
+ * let prog =  (Case (App (Abs ("x", Var "x"), (IntExpr 5)), [
+ *              Branch ((IntExprp 5), (BinOp (Add, (IntExpr 1, IntExpr 1))));
+ *              Branch (Constrp ("List", [Varp "x"; Varp "y"]), (Var "x"))])
+ *              );;
+ *
+ * prettyprint prog;;
+ *
+ * outputs:
+ *
+ * (case (fun x = x) (5) of  
+ * [5 -> (1 + 1); List(x, y) -> x])
+ *)
+
 open Cbninterp
 
-let str_op op = match op with
+let strop op = match op with
     | Add   -> "+"
     | Subtr -> "-"
     | Mult  -> "*"
@@ -15,16 +33,22 @@ and patlist p = match p with
     | x::[] -> strpat x
     | x::xs -> strpat x ^ ", " ^ patlist xs
 
+(* Main function *)
 let rec prettyprint program = 
     print_string (strdef program ^ "\n")
 
+(* 
+ * Case analysis on the input constructor.
+ * Depending on the case, either calls itself recursively or
+ * uses a helper function.
+ *) 
 and strdef p = match p with 
     | IntExpr n -> string_of_int n
     | Var x -> x
     | Constructor (name, elist) -> name ^ "(" ^ (strlist elist) ^ ")"
     | BinOp (operator, (op1, op2)) -> 
         "(" ^ (strdef op1) ^ 
-        " " ^ (str_op operator) ^ 
+        " " ^ (strop operator) ^ 
         " " ^ (strdef op2) ^ ")"
     | Abs (vname, body) -> "fun " ^ vname ^ " = " ^ (strdef body)
     | App (e1, e2) -> "(" ^ (strdef e1) ^ ") (" ^ (strdef e2) ^ ")"
