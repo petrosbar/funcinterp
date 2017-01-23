b7a66f82db225e0aee9777c04d423c71692f1b15
Petros Barbagiannis <pbarb@math.uoa.gr>
2017-01-23 16:40:15 +0200

    Added value type and modified eval accordingly.

--- OCaml/cbninterp.ml
+++ OCaml/cbninterp.ml
@@ -13,12 +13,17 @@ type expr = IntExpr of int
           | Abs of (name * expr)
           | App of (expr * expr)
           | Case of (expr * (branch list))
-          | Thunk of (env * name * expr)
 
 and  branch = Branch of (pattern * expr)
 
-and env = Env of ((name * expr) list)
+type value = IntVal of int
+           | Thunk of (env * name * expr)
+           | ConstrVal of (name * (expr list))
 
+and env = Env of ((name * value) list)
+
+
+(* TODO: Use List.combine *)
 let rec zip xs ys = match xs, ys with
     | [], _ -> []
     | _, [] -> []
@@ -29,49 +34,55 @@ let rec extractFromList xs = match xs with
     | _ -> []
 
 
+(* val eval : env -> expr -> value *)
 let rec eval env = function
-    | IntExpr n -> IntExpr n
-    | Var x -> eval env (List.assoc x env)
+    | IntExpr n -> IntVal n
+    | Var x -> List.assoc x env
     | Abs (n, ex) -> Thunk ((Env env), n, ex)
     | App (ex1, ex2) -> 
         begin match eval env ex1 with
-            | Thunk (Env en, n, ex) -> eval ((n, ex2) :: en) ex
+            | Thunk (Env en, n, ex) ->
+                (* Insert ex2 as a (unevaluated) thunk into the environment... *)
+                let thunk = Thunk ((Env env), n, ex2)
+                (* ... and evaluate ex2. *)
+                in eval ((n, thunk) :: en) ex
             | _ -> failwith "Invalid function application"
-        end
-    | Constructor (cname, explist) -> Constructor (cname, explist)
+        end 
+    | Constructor (cname, explist) -> ConstrVal (cname, explist)
     | Case (ex, (brlist)) -> let evex = eval env ex in
                              matchPattern env evex brlist
     | BinOp (operator, (op1, op2)) -> 
         let x = eval env op1 in
         let y = eval env op2 in
             begin match (x, y) with 
-                (IntExpr o1, IntExpr o2) -> IntExpr (binOp operator o1 o2)
-                | _ -> failwith "das"
+                (IntVal o1, IntVal o2) -> IntVal (binOp operator o1 o2)
+                | _ -> failwith "Invalid binary operands"
             end
     | _ -> failwith "Unmatched" 
 
-and binOp oprtr op1 op2 =
-    match oprtr with
-        | Add   -> op1 + op2
-        | Subtr -> op1 - op2
-        | Mult  -> op1 * op2
+(* val binOp : operator -> int -> int -> int *)
+and binOp oprtr op1 op2 = match oprtr with
+    | Add   -> op1 + op2
+    | Subtr -> op1 - op2
+    | Mult  -> op1 * op2
 
+(* val matchPattern : env -> expr -> branch list -> value *)
 and matchPattern env exp blist = match blist with
     | br::brlist -> 
         begin match exp with
-            | Constructor (name, explist) as constr->
+            | ConstrVal (name, explist) as constr->
                 begin match br with
                     | Branch (IntExprp n, _) -> matchPattern env constr brlist
                     | Branch (Constrp (cname, clist), cexpr) ->
 	                    if cname = name then 
-                            let varlist = zip (extractFromList clist) explist in
+                            let varlist = List.combine (extractFromList clist) (evalListExpr env explist) in
 		                    let env1 = List.append env varlist in
                             eval env1 cexpr
                         else 
                             matchPattern env constr brlist
                     | Branch (Varp vname, vexpr) -> eval ((vname, constr) :: env) vexpr      
                 end
-            | IntExpr n as intexpr ->
+            | IntVal n as intexpr ->
                 begin match br with
                     | Branch (IntExprp p, expr) -> 
                         if n = p then
@@ -87,6 +98,14 @@ and matchPattern env exp blist = match blist with
         end
     | [] -> failwith "Unmatched"
 
+(* 
+ * XXX: This should not be here. Add an interface file and put function
+ * signatures in it.
+ *)
+and evalListExpr env exprlist = match exprlist with
+    | (x::xs) -> eval env x :: evalListExpr env xs
+    | []      -> []
+
 
 let test1 = 
     eval [] (BinOp (Add, (IntExpr 4, IntExpr 2)));;
@@ -95,10 +114,10 @@ let test2 =
     eval [] (Abs ("x", (BinOp (Add, (IntExpr 1, IntExpr 2)))));;
 
 let test3 = 
-    eval [("x", IntExpr 2)] (Abs ("x", (BinOp (Add, (Var "x", IntExpr 2)))));;
+    eval [("x", IntVal 2)] (Abs ("x", (BinOp (Add, (Var "x", IntExpr 2)))));;
 
 let test4 = 
-    eval [("y", IntExpr 2)] 
+    eval [("y", IntVal 2)] 
         (App  (Abs ("x", BinOp (Add, (Var "x", IntExpr 2))), IntExpr 10));;
 
 (*
