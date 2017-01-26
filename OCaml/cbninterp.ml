95885f6457289adcacb404e5a8d4d8817546d7c2
petrosbar <pbarb@math.uoa.gr>
2017-01-26 16:56:25 +0200

    Comments

--- OCaml/cbninterp.ml
+++ OCaml/cbninterp.ml
@@ -63,29 +63,45 @@ let rec listOfThunks env exprlist = match exprlist with
 let rec eval env = function
     | IntExpr n -> IntVal n
     (* 
-     * A variable holds either an integer value or a thunk (i.e., a λ_.e).
-     * In the first case, return the value whereas in the second one
+     * A variable is bound to either an integer value or a thunk (i.e., a λ_.e).
+     * In the first case, just return the value while in the second
      * return the evaluated expression of the thunk.
      *)
     | Var x -> 
         begin match List.assoc x env with
             | IntVal n -> IntVal n
-            | Thunk (Env env, undrst, expr) -> eval env expr
+            | Thunk (Env env, str, expr) -> eval env expr
+            | ConstrVal (name, explist) as const -> const
         end
+    (* The evaluation of an abstraction is a thunk. *)
     | Abs (n, ex) -> Thunk ((Env env), n, ex)
+    (* Function application. *)
     | App (ex1, ex2) ->
         begin match eval env ex1 with
+            (* Make sure ex1 evaluates to a thunk... *)
             | Thunk (Env en, n, ex) ->
+                (* ...and build a second one with ex2. *)
                 let thunk = Thunk ((Env env), "_", ex2)
-                (* Insert ex2 as a (unevaluated) thunk into the environment and evaluate ex1. *)
+                (* 
+                 * Finally, insert the second thunk into the environment and evaluate ex1. 
+                 * If during ex1's evaluation ex2's value is needed, then (and only then) evaluate it.
+                 *)
                 in eval ((n, thunk) :: en) ex
             | _ -> failwith "Invalid function application"
-        end 
+        end
+    (* Constructor evaluation. *)
     | Constructor (cname, explist) ->
         (* Return the constructor with its arguments evaluated. *)
-        let vallist = listEval env explist in
-            ConstrVal (cname, vallist)
+        let vallist = listEval env explist
+        in ConstrVal (cname, vallist)
+    (* Case expression evaluation. *)
     | Case (ex, (brlist)) ->
+        (*
+         * The argument of a case expression must be brought into Weak Head Normal Form 
+         * before calling matchPattern. Thus, if it is a constructor turn its enclosed list
+         * of arguments/expressions into a list of thunks but do not evaluate any of them,
+         * and call matchPattern on it. Anything else follows the usual reduction rules.
+         *)
         begin match ex with
             | Constructor (cname, explist) -> 
                 let thunklist = listOfThunks env explist in
@@ -95,6 +111,7 @@ let rec eval env = function
                 let evex = eval env ex in
                 matchPattern env evex brlist
         end
+    (* Binary operation evaluation. *)
     | BinOp (operator, (op1, op2)) -> 
         let x = eval env op1 in
         let y = eval env op2 in
@@ -118,6 +135,10 @@ and binOp oprtr op1 op2 = match oprtr with
     | Mult  -> op1 * op2
 
 
+(*
+ * matchPattern evaluates the expression part of the branch whose pattern matches the case argument.
+ *
+ *) 
 (* val matchPattern : env -> value -> branch list -> value *)
 and matchPattern env casexp blist = match blist with
     | br::brlist -> 
@@ -156,6 +177,8 @@ and matchPattern env casexp blist = match blist with
     | [] -> failwith "Unmatched"
 
 
+(* Test Case (TODO: They should be in a separate file) *)
+
 (* 4 + 2 *)
 let test1 = 
     eval [] (BinOp (Add, (IntExpr 4, IntExpr 2)));;
@@ -164,23 +187,29 @@ let test1 =
 let test2 = 
     eval [] (Abs ("x", (BinOp (Add, (IntExpr 1, IntExpr 2)))));;
 
-
+(* λx.x + 2 *)
 let test3 = 
     eval [("x", IntVal 2)] (Abs ("x", (BinOp (Add, (Var "x", IntExpr 2)))));;
 
+(* (λx.x + 2) 10 *)
 let test4 = 
     eval [("y", IntVal 2)] 
         (App  (Abs ("x", BinOp (Add, (Var "x", IntExpr 2))), IntExpr 10));;
 
 
 (*
- * (λx.x x) (λx.x x) [Omega]
+ * Omega, DOES NOT HALT. (λx.x x) (λx.x x)
  *
  *let test5 = 
  *  eval []
  *       (App (Abs ("x", App (Var "x", Var "x")), Abs ("x", App (Var "x", Var "x"))));;
  *)
 
+(*
+ * case (λx.x) 5 of
+ *   5          -> 1 + 1
+ *   List(x, y) -> x
+ *)
 let test6 = 
     eval []
         (Case (App (Abs ("x", Var "x"), (IntExpr 5)), [
@@ -188,6 +217,11 @@ let test6 =
             Branch (Constrp ("List", [Varp "x"; Varp "y"]), (Var "x"))])
         );;
 
+(*
+ * case List((λx.x) 5) of
+ *   List(y)    -> y
+ *   5          -> 1 + 1
+ *)
 let test7 = 
     eval []
         (Case (Constructor ("List", [App (Abs ("x", (Var "x")), (IntExpr 5))]), [
@@ -210,11 +244,21 @@ let test8 =
             Branch (IntExprp 5, BinOp (Add, (IntExpr 1, IntExpr 1)))])
         );;
 
-
+(* 
+ * DOES NOT HALT!
+ *
+ * case List omega of
+ *   List y -> y
+ *   5      -> 1 + 1
+ *)
+(*
 let test9 = 
     eval []
-        (Case (Constructor ("List", [((App (Abs ("x", App (Var "x", Var "x")), Abs ("x", App (Var "x", Var "x")))))]), [
-            Branch (Constrp ("List", [Varp "y"]), (Var "y"));
+        (Case 
+            (Constructor ("List", [((App 
+                (Abs ("x", App (Var "x", Var "x")), 
+                 Abs ("x", App (Var "x", Var "x")))))]), 
+           [Branch (Constrp ("List", [Varp "y"]), (Var "y"));
             Branch (IntExprp 5, BinOp (Add, (IntExpr 1, IntExpr 1)))])
         );;
-
+*)
